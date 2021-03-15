9c7a88f5c8e5346b81d52a30c0acb32f8313630b
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-03-15 16:34:54 +0700

    Everyone can restart the bot until fix session issue

--- deegram/plugins/restart.py
+++ deegram/plugins/restart.py
@@ -17,7 +17,7 @@ if path.exists('restart.pickle'):
     remove('restart.pickle')
 
 
-@bot.on(NewMessage(pattern='/restart', from_users=OWNER_ID))
+@bot.on(NewMessage(pattern='/restart'))
 async def restart(event):
     restart_message = await event.reply("Restarting, Please wait!")
     with open('restart.pickle', 'wb') as status:
